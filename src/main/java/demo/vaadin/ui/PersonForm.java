131181e702ec88eccdc035710f815d337d2b36f0
oleg.pahhomov <oleg.pahhomov@gmail.com>
2015-11-24 23:17:50 +0200

    UI better input

--- src/main/java/demo/vaadin/ui/PersonForm.java
+++ src/main/java/demo/vaadin/ui/PersonForm.java
@@ -15,7 +15,7 @@ public class PersonForm extends AbstractForm<Person> {
     private TextField score;
 
     public PersonForm(Person person) {
-        fullName = new MTextField("Full name");
+        fullName = new MTextField("Person's full name");
         examName = new MTextField("Name of the exam");
         examCode = new MTextField("Exam code (7 symbols)").withInputPrompt("XXX-ZZZ").withWidth(100, Unit.PIXELS);
         score = new MTextField("Exam score").withInputPrompt("YYY").withWidth(50, Unit.PIXELS);
